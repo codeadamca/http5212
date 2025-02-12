692d535b0c7a2ee8d6d6b2dc1dbf6adb7808d00d
Adam <thomasadam83@hotmail.com>
2025-02-12 09:43:49 -0500

    Reorganized files

---
+++ week-1/colours.sql
@@ -0,0 +1,10 @@
+CREATE TABLE colours (
+  id int NOT NULL,
+  name varchar(255) NOT NULL,
+  rgb varchar(255) NOT NULL
+);
+
+INSERT INTO colours (id, name, rgb) VALUES
+(1, 'Red', 'ff0000'),
+(2, 'Blue', '0000ff'),
+(3, 'Green', '00ff00');
